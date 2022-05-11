c49c993b0cce190302af5994655ba187a7b1564f
Stefan Mares <stefanutmares@gmail.com>
2022-05-11 16:43:07 +0300

    Updated divider

--- devbuddy/src/components/Divider/Divider.tsx
+++ devbuddy/src/components/Divider/Divider.tsx
@@ -1,9 +1,61 @@
 import React from "react";
 import styles from "./Divider.module.scss";
 
+type DividerStyle = {
+  lineStyle?: {
+    borderColor: string;
+    borderWidth: string;
+  };
+  height?: string;
+  text?: {
+    color?: string;
+    fontSize?: string;
+    fontWeight?: string;
+    spacing?: string;
+  };
+};
+
 interface DividerProps {
-  style?: React.CSSProperties;
+  style?: DividerStyle;
+  text?: string;
 }
-export default function Divider({ style }: DividerProps) {
-  return <div className={styles.divider} style={style}></div>;
+export default function Divider({ style, text }: DividerProps) {
+  return (
+    <>
+      {!text && (
+        <div
+          className={styles.divider}
+          style={{
+            border: `${style?.lineStyle?.borderWidth} solid ${style?.lineStyle?.borderColor}`,
+            height: style?.height,
+          }}
+        ></div>
+      )}
+      {text && (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+            alignItems: "center",
+            height: style?.height,
+          }}
+        >
+          <div className={styles.divider} style={{ width: "50%" }}></div>
+          <span
+            style={{
+              marginLeft: style?.text?.spacing,
+              marginRight: style?.text?.spacing,
+              fontSize: style?.text?.fontSize,
+              fontWeight: style?.text?.fontWeight,
+              color: style?.text?.color,
+            }}
+          >
+            {text}
+          </span>
+          <div className={styles.divider} style={{ width: "50%" }}></div>
+        </div>
+      )}
+    </>
+  );
 }
